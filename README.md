ec303878572257d9fb193cf246766c52a9e30b2a
Rafael Barrelo <rafaelbarrelo@gmail.com>
2015-03-20 00:44:23 -0300

    imagem do travis e correcao da build

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # travis-ci-test
+[![Build Status](https://travis-ci.org/rafaelbarrelo/travis-ci-test.svg)](https://travis-ci.org/rafaelbarrelo/travis-ci-test)
--- travis_sample.py
+++ travis_sample.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Quebra a build!!!'
+    return 'Hello travis!!!'
 
 if __name__ == '__main__':
     app.run()
